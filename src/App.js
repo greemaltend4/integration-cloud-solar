de466083eb5f3e3fcf9d0e30b3bd2482e7b12221
greemaltend4 <dengestabiy@hotmail.com>
2025-11-06 12:13:36 +0000

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tableau de bord Solar</h1>
+      </header>
+      <main>
+        <p>Bienvenue dans votre tableau de bord personnalisé pour les systèmes solaires.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
