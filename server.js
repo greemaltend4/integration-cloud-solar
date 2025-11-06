9c9050cf35386abd632d1db86848358544c03eb0
greemaltend4 <dengestabiy@hotmail.com>
2025-11-06 12:16:40 +0000

    Update welcome message to clarify API purpose

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Cloud Integration API for Solar Systems');
+    res.send('Welcome to the Solar Systems Cloud Integration API');
 });
 
 app.listen(PORT, () => {
