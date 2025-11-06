e38f0088cddd861e9625decc81891fcc4ad6ffec
greemaltend4 <dengestabiy@hotmail.com>
2025-11-06 12:13:33 +0000

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l'API de l'intégration Cloud pour Systèmes Solaires');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
